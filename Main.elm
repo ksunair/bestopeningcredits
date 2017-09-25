384db798910f04aa2884856c00e269039edd26b2
Unni <ksunair@yahoo.com>
2017-09-24 21:49:08 -0500

    Added initial data

--- Main.elm
+++ Main.elm
@@ -3,5 +3,14 @@ module Main exposing (..)
 import Html exposing (..)
 
 
+initData =
+    [ { title = "Game of Thrones"
+      , url = "https://www.youtube.com/watch?v=s7L2PVdrb_8"
+      , votes = 10
+      , userCount = 1
+      }
+    ]
+
+
 main =
-    text "Hello World"
+    text <| toString <| initData
